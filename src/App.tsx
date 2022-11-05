ab87e0275c36ec0dc49a2f4e9857f4076399d062
mvikse <maren.vikse@gmail.com>
2022-11-05 18:43:02 +0100

    Lagt inn ferdig brett

--- src/App.tsx
+++ src/App.tsx
@@ -29,20 +29,20 @@ const MinKomponent = ({ navn }: Props) => {
   return <div>hei på deg, {navn}...</div>;
 };
 
-/*
-Ø  K  Ø  X  I  T  V  A  H  E  P  E  B  N  Y
-K  A  C  O  U  H  S  V  G  W  B  E  C  D  Ø
-F  A  K  W  Z  P  A  K  X  Z  Z  N  H  V  Ø
-E  C  S  M  I  U  I  V  X  Ø  J  Z  Ø  C  Q
-I  Z  C  G  M  K  Z  Å  X  G  V  Y  Æ  D  B
-B  X  D  B  D  T  M  Z  K  I  A  O  X  W  M
-V  K  X  X  L  Y  G  W  M  D  U  G  S  Ø  T
-I  J  F  W  Æ  G  S  F  M  V  K  X  U  U  O
-Y  X  K  Q  L  T  B  F  L  R  Y  X  R  G  Y
-T  U  H  G  O  Q  T  Å  V  Z  G  J  X  E  A
-W  R  J  K  H  C  W  J  J  T  T  X  G  J  V
-G  M  K  Y  S  F  Z  G  N  O  Ø  C  Z  N  Q
-S  Q  M  S  R  A  X  H  O  D  T  O  Æ  E  A
-L  W  B  I  Å  W  Æ  R  Ø  C  N  M  U  S  Ø
-L  J  X  Ø  Æ  Ø  Y  Æ  B  G  G  H  U  Æ  Ø
-*/
+const brett = [
+  ['Ø', 'K', 'Ø', 'X', 'I', 'T', 'V', 'A', 'H', 'E', 'P', 'E', 'B', 'N', 'Y'],
+  ['K', 'A', 'C', 'O', 'U', 'H', 'S', 'V', 'G', 'W', 'B', 'E', 'C', 'D', 'Ø'],
+  ['F', 'A', 'K', 'W', 'Z', 'P', 'A', 'K', 'X', 'Z', 'Z', 'N', 'H', 'V', 'Ø'],
+  ['E', 'C', 'S', 'M', 'I', 'U', 'I', 'V', 'X', 'Ø', 'J', 'Z', 'Ø', 'C', 'Q'],
+  ['I', 'Z', 'C', 'G', 'M', 'K', 'Z', 'Å', 'X', 'G', 'V', 'Y', 'Æ', 'D', 'B'],
+  ['B', 'X', 'D', 'B', 'D', 'T', 'M', 'Z', 'K', 'I', 'A', 'O', 'X', 'W', 'M'],
+  ['V', 'K', 'X', 'X', 'L', 'Y', 'G', 'W', 'M', 'D', 'U', 'G', 'S', 'Ø', 'T'],
+  ['I', 'J', 'F', 'W', 'Æ', 'G', 'S', 'F', 'M', 'V', 'K', 'X', 'U', 'U', 'O'],
+  ['Y', 'X', 'K', 'Q', 'L', 'T', 'B', 'F', 'L', 'R', 'Y', 'X', 'R', 'G', 'Y'],
+  ['T', 'U', 'H', 'G', 'O', 'Q', 'T', 'Å', 'V', 'Z', 'G', 'J', 'X', 'E', 'A'],
+  ['W', 'R', 'J', 'K', 'H', 'C', 'W', 'J', 'J', 'T', 'T', 'X', 'G', 'J', 'V'],
+  ['G', 'M', 'K', 'Y', 'S', 'F', 'Z', 'G', 'N', 'O', 'Ø', 'C', 'Z', 'N', 'Q'],
+  ['S', 'Q', 'M', 'S', 'R', 'A', 'X', 'H', 'O', 'D', 'T', 'O', 'Æ', 'E', 'A'],
+  ['L', 'W', 'B', 'I', 'Å', 'W', 'Æ', 'R', 'Ø', 'C', 'N', 'M', 'U', 'S', 'Ø'],
+  ['L', 'J', 'X', 'Ø', 'Æ', 'Ø', 'Y', 'Æ', 'B', 'G', 'G', 'H', 'U', 'Æ', 'Ø']
+]
